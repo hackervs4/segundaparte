8bf21ca7e06e70fab707a50b250ae4ebf15f128b
Alexandre Miranda <alexandre.mm@outlook.com>
2017-02-21 12:10:41 -0300

    Começando a programação da movimentação da prancha
    Aula 6

---
+++ Assets/Scripts/PranchaMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PranchaMove : MonoBehaviour {
+
+    public float velocidade;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 velocidadevetorial = Vector3.left * velocidade;
+
+        transform.position = transform.position + velocidadevetorial * Time.deltaTime;
+
+       
+        }
+ 
+}
